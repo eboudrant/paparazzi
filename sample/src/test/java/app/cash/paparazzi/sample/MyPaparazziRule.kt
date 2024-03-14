c97ff8bc83c96e09f189b9c7305bb284cc9f0e1c
Emmanuel Boudrant <eboudrant@netflix.com>
2024-03-14 10:38:07 -0700

    repro step

---
+++ sample/src/test/java/app/cash/paparazzi/sample/MyPaparazziRule.kt
@@ -0,0 +1,41 @@
+package app.cash.paparazzi.sample
+
+import androidx.compose.runtime.Composable
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.accessibility.AccessibilityRenderExtension
+import app.cash.paparazzi.Paparazzi
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class MyPaparazziRule : TestRule {
+
+  private val normal = Paparazzi(
+    deviceConfig = DeviceConfig.PIXEL,
+    renderExtensions = setOf(AccessibilityRenderExtension())
+  )
+
+  private val scaled = Paparazzi(
+    deviceConfig = DeviceConfig.PIXEL.copy(fontScale = 2f),
+    renderExtensions = setOf(AccessibilityRenderExtension())
+  )
+
+  override fun apply(base: Statement, description: Description): Statement {
+    val normalStatement = normal.apply(base, description)
+    val scaledStatement = scaled.apply(base, description)
+    return object : Statement() {
+      override fun evaluate() {
+        normalStatement.evaluate()
+        scaledStatement.evaluate()
+      }
+    }
+  }
+
+  fun snapshot(
+    name: String? = null,
+    content: @Composable () -> Unit,
+  ) {
+    normal.snapshot("${name.orEmpty()}-normal", content)
+    scaled.snapshot("${name.orEmpty()}-scaled", content)
+  }
+}
---
+++ sample/src/test/java/app/cash/paparazzi/sample/ScaledTextTest.kt
@@ -0,0 +1,18 @@
+package app.cash.paparazzi.sample
+
+import androidx.compose.material.Text
+
+import org.junit.Rule
+import org.junit.Test
+
+class ScaledTextTest {
+  @get:Rule
+  val paparazzi = MyPaparazziRule()
+
+  @Test
+  fun compositeItems() {
+    paparazzi.snapshot {
+      Text("Text")
+    }
+  }
+}
